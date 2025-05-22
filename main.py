cf48469905ef4c4fddeca53ee7430b41291e5899
Maxence Rose <pirmax@icloud.com>
2025-05-22 18:27:57 +0200

    Fix EPD initialization in run_epaper function for consistency

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def animation_sequence(display_fn, touch_check_fn=None):
 
 def run_epaper():
     epd = EPD()
-    epd.init()
+    epd.init(1)
     epd.Clear(0xFF)
     epd.displayPartBaseImage(epd.getbuffer(Image.new('1', (SCREEN_WIDTH, SCREEN_HEIGHT), 255)))
 
